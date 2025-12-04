de61b506facfbdba8bd6d76341a13a422d81475d
Utso Sarkar <utsosarkar7870@gmail.com>
2025-12-04 17:34:41 +0600

    build error solve

--- components/reusable/CustomButton.tsx
+++ components/reusable/CustomButton.tsx
@@ -1,8 +1,8 @@
 
-function CustomButton({children}) {
+function CustomButton({children}:{children:React.ReactNode}) {
   return (
     <div className=" text-center mt-3">
-      <button className=' px-6 py-[14px] cursor-pointer bg-secondaryColor rounded-xl text-blackColor font-medium text-lg'>{children}</button>
+      <button className=' px-6 py-3.5 cursor-pointer bg-secondaryColor rounded-xl text-blackColor font-medium text-lg'>{children}</button>
     </div>
   )
 }
